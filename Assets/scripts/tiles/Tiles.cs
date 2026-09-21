19091fe6ee15741ec439d5dc3165dd9db881d200
bitch <169540526+domeDiff@users.noreply.github.com>
2026-09-21 22:35:31 +0530

    tile_prefab

--- Assets/scripts/tiles/Tiles.cs
+++ Assets/scripts/tiles/Tiles.cs
@@ -2,15 +2,41 @@ using UnityEngine;
 
 public class Tiles : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public int tileType;
+    public Vector2Int boardPosition;
+
+    public void Setup(int type, Vector2Int position)
     {
-        
+        tileType = type;
+        boardPosition = position;
+
+        SetColor();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetColor()
     {
-        
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        switch (tileType)
+        {
+            case 0:
+                spriteRenderer.color = Color.magenta; break;
+
+            case 1:
+                spriteRenderer.color = Color.cyan; break;
+
+            case 2:
+                spriteRenderer.color = Color.pink; break;
+
+            case 3:
+                spriteRenderer.color = Color.violet; break;
+
+            case 4:
+                spriteRenderer.color = Color.red; break;
+
+            case 5:
+                spriteRenderer.color = Color.yellowNice; break;
+
+        }
     }
 }
